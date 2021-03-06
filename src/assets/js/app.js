e06a55149dcc763e126f44453f42723944ab888e
tdubsxc <traviedubs@gmail.com>
2021-03-06 17:30:48 -0500

    Added a sticky header.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,7 @@
 import RevealOnScroll from './components/RevealOnScroll';
+import StickyHeader from './components/StickyHeader';
 
 new RevealOnScroll('.features__item--fade-left', 'left', 1000, '25rem', 250);
 new RevealOnScroll('.features__item--fade-right', 'right', 1000, '25rem', 250);
 new RevealOnScroll('.testimonials__item', 'bottom', 1000, '25rem', 250);
+new StickyHeader();
